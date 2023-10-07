28e3973055fd0c0772e3d8844029a3aee89c1cb9
Krish Patel <krishpkreame1405+github@gmail.com>
2023-10-08 00:40:59 +1300

    Dymanic Market RSI APIS, on demand.

--- tradingview.py
+++ tradingview.py
@@ -1,120 +1,152 @@
 # Environment variables and datetime
+import re
 import os
 import time
+from datetime import datetime
 # For web scraping
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 # For waiting for web elements to load instead of time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import InvalidSessionIdException
+# For MySQL database
+import json
+import pymysql
 
 
 class API:
     # Initialize class variables and environment variables
-    def __init__(self, __keywords=None):
-        self.__account_name = "Quality Kiwi Shop Ltd"
-        self.tv_started = False  # Flag to check if Trading View is running
+    def __init__(self):
+        # Get DB info from environment variable
+        self.dbconf = json.loads(os.environ.get("MYSQL_DB1_JSON_CONN", ""))
+        # Set up flags for services, first connect to database
+        self.connection = pymysql.connect(
+            host=self.dbconf["host"],
+            user=self.dbconf["user"],
+            password=self.dbconf["password"],
+            db=self.dbconf["database"],
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+        # Get symbol names from database
+        with self.connection.cursor() as cursor:
+            _sql = f"SELECT * FROM `input_info`"
+            cursor.execute(_sql)
+            # Set up flags for services of each market
+            self.tv_market_data = {
+                row['tv_symbol']: {
+                    'status': False,
+                    'link': row['tv_link'],
+                    'instance': None
+                } for row in cursor}
+        # Close the temp connection
+        self.connection.close()
+
         # Get Selenium URL from environment variable
         self.selenium_url = os.environ.get("SELENIUM_URL", "")
-
+        # Set up Selenium options
         self.browser_options = webdriver.ChromeOptions()
         self.browser_options.add_argument("--start-maximized")
-        self.cmc_url = "file:///docker/selenium/asx200.html"
 
-    def wait_for_element(self, xpath_str, seconds=60):
+    def start_market_service(self, __tvsymbol):
         try:
-            # Wait for the element to load (default timeout is 60 seconds)
-            WebDriverWait(self.cmc, seconds).until(
-                EC.presence_of_element_located((By.XPATH, xpath_str)))
-            self.cmc.implicitly_wait(3)
-            return True
-        except Exception as e:  # If the element does not load, catch exception
-            print(e)
-            return False
+            if __tvsymbol not in self.tv_market_data:
+                raise Exception(
+                    f"TradingView {__tvsymbol} is not supported. (start)")
+            if not self.tv_market_data[__tvsymbol]["status"]:
+                self.tv_market_data[__tvsymbol]["status"] = True
+                self.tv_market_data[__tvsymbol]["instance"] = webdriver.Remote(
+                    command_executor=self.selenium_url,
+                    options=self.browser_options)
+                self.tv_market_data[__tvsymbol]["instance"].get(
+                    self.tv_market_data[__tvsymbol]["link"])
+                WebDriverWait(self.tv_market_data[__tvsymbol]["instance"], 20).until(
+                    EC.presence_of_element_located((By.XPATH, '//div[@data-name="legend-source-item"]')))
 
-    def start_service(self):  # Start CMC Markets
-        if not self.cmc_started:  # If CMC is not running, start it
-            print(" service started.")
-            self.cmc_started = True
+                self.tv_market_data[__tvsymbol]["instance"].implicitly_wait(1)
 
-            # Start the browser
-            self.cmc = webdriver.Remote(
-                command_executor=self.selenium_url,
-                options=self.browser_options)
-            # Go to the CMC Markets login page
-            self.cmc.get(self.cmc_url)
-            # Wait for the page to load and zooming out
-            self.cmc.execute_script("document.body.style.zoom='50%'")
-            # Wait for the login page to load
-            if self.wait_for_element('//*[@id="username"]'):
-                print("Login page loaded.\n--- waiting for selection...")
+                temp_sessionid = str(
+                    self.tv_market_data[__tvsymbol]["instance"].session_id)
+
+                return f"{__tvsymbol} service started. {temp_sessionid}"
             else:
-                raise Exception("CMC login page failed to load.")
+                temp_sessionid = str(
+                    self.tv_market_data[__tvsymbol]["instance"].session_id)
+                raise Exception(
+                    f"TradingView {__tvsymbol} service is already running. {temp_sessionid}")
+        except InvalidSessionIdException as e:
+            self.tv_market_data[__tvsymbol]["status"] = False
+            self.tv_market_data[__tvsymbol]["instance"] = None
+            raise InvalidSessionIdException(e)
 
-            # Login to CMC Markets
-            self.type_element('//*[@id="username"]', self.username)
-            self.type_element('//*[@id="password"]', self.password)
+    def get_rsi(self, __tvsymbol):
+        try:
+            if __tvsymbol not in self.tv_market_data:
+                raise Exception(
+                    f"TradingView {__tvsymbol} is not supported. (get rsi)")
+            if self.tv_market_data[__tvsymbol]["status"]:
+                self.raw_time = self.tv_market_data[__tvsymbol]["instance"].find_element(
+                    By.XPATH, '//button[@data-name="time-zone-menu"]').text.split("\n")[-1]
 
-            # Click login button
-            self.click_element("""
-                /html/body/div[1]/div/cmc-login/div/section/div[1]/div[1]/form/div[4]/input
-                """)
+                self.raw_rsi_value = self.tv_market_data[__tvsymbol]["instance"].find_element(
+                    By.XPATH, '//div[@data-name="legend-source-item"]').text.split("\n")[-1]
 
-            try:
-                # Wait for the account select page to load
-                if self.wait_for_element("""
-                    /html/body/div[1]/div/div/div/div/section/cmc-account-options/div/div[3]/div[1]/button[2]
-                    """, seconds=25):
-                    print("CMC account tab switch needed.")
-                else:
-                    raise TimeoutError
-                # Select the account
-                self.click_element("""
-                    /html/body/div[1]/div/div/div/div/section/cmc-account-options/div/div[3]/div[1]/button[2]
-                    """)
-            except TimeoutError:
-                print("CMC account tab switch not needed.")
-                pass
+                self.raw_price_price = self.tv_market_data[__tvsymbol]["instance"].find_element(
+                    By.XPATH, '//div[@data-name="legend-series-item"]').text.split("\n")[-1]
 
-            self.cmc.implicitly_wait(15)
+                self.time_step1 = self.raw_time.split(" ")[0]
+                self.time_step2 = datetime.strptime(
+                    self.time_step1, "%H:%M:%S").time()
+                self.datetime_value = str(datetime.combine(
+                    datetime.now().date(), self.time_step2))
 
-            # Sometimes CMC asks for account selection, if needed select the account otherwise, wait for 20 second timeout
-            try:
-                # Wait for the account select page to load
-                span_list = self.cmc.find_elements(By.TAG_NAME, "span")
-                for span in span_list:
-                    if self.__account_name in span.text:
-                        span.click()
-                        print("CMC account selection and account selected")
-                        raise ValueError
-                raise TimeoutError("Account not found")
-            except ValueError:
-                pass
+                self.rsi_value = float(self.raw_rsi_value)
 
-            # if not wait and try again
-            for time_ran in range(10):
-                if self.cmc_loggedin == True:  # If the flag is already True, break the loop
-                    break
-                # Wait for the main page to load (cant use wait_for_element because the elements are unique)
-                time.sleep(30)
-                if len(self.cmc.find_elements(By.CLASS_NAME, 'news-list-item')) > 399:
-                    print("The main page news loaded.")
-                    self.cmc_loggedin = True  # Set the flag to True
-                else:
-                    print(
-                        "The main page news failed to load, trying again. attempt: " + str(time_ran))
-            if not self.cmc_loggedin:
-                raise Exception("CMC News failed to load in.")
-        else:
-            # Raise exception if CMC is already running
-            raise Exception("CMC News service is already running.")
+                self.price_step1 = [float(value)
+                                    for value in re.findall('\d+\.\d+', self.raw_price_price)][0:4]
 
-    def stop_service(self):
-        if self.cmc_started:  # If CMC is running, stop it
-            try:
-                self.cmc.quit()  # Close the browser
-                self.cmc_started = False  # Set the flag to False
-            except Exception as e:
-                print(e)
-        else:  # If CMC is not running, raise exception
-            raise Exception("CMC News service is not running.")
+                self.price_values = {
+                    "open": self.price_step1[0],
+                    "high": self.price_step1[1],
+                    "low": self.price_step1[2],
+                    "close": self.price_step1[3]
+                }
+
+                time.sleep(1)
+                temp_sessionid = str(
+                    self.tv_market_data[__tvsymbol]["instance"].session_id)
+                self.final_data = {
+                    "rsi": self.rsi_value,
+                    "datetime": self.datetime_value,
+                    "tv_symbol": __tvsymbol,
+                    "price": self.price_values,
+                    "sessionid": temp_sessionid
+                }
+
+                return self.final_data
+            else:
+                raise Exception(
+                    f"TradingView {__tvsymbol} service is not running, can not get rsi.")
+        except InvalidSessionIdException as e:
+            self.tv_market_data[__tvsymbol]["status"] = False
+            self.tv_market_data[__tvsymbol]["instance"] = None
+            raise InvalidSessionIdException(e)
+
+    def stop_market_service(self, __tvsymbol):
+        try:
+            if __tvsymbol not in self.tv_market_data:
+                raise Exception(
+                    f"TradingView {__tvsymbol} is not supported. (stop)")
+            if self.tv_market_data[__tvsymbol]["status"]:
+                temp_sessionid = str(
+                    self.tv_market_data[__tvsymbol]["instance"].session_id)
+                self.tv_market_data[__tvsymbol]["instance"].quit()
+                self.tv_market_data[__tvsymbol]["status"] = False
+                return f"{__tvsymbol} service stopped. {temp_sessionid}"
+            else:
+                raise Exception(
+                    f"TradingView {__tvsymbol} service is not running.")
+        except InvalidSessionIdException as e:
+            self.tv_market_data[__tvsymbol]["status"] = False
+            self.tv_market_data[__tvsymbol]["instance"] = None
+            raise InvalidSessionIdException(e)

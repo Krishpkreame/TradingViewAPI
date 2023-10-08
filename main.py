6314cd744aea48944d06bfb9c77f05e7190b4a8d
Krish Patel <krishpkreame1405+github@gmail.com>
2023-10-08 15:40:58 +1300

    Added comments, and force start implemented

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
-import re
-import os
-import time
-from datetime import datetime
-from flask import Flask, jsonify
-import tradingview as tv
-from selenium.common.exceptions import InvalidSessionIdException
+from flask import Flask, jsonify  # For HTTP API
+import tradingview as tv  # For TradingView API
+from selenium.common.exceptions import InvalidSessionIdException  # For Selenium Error
 
-app = Flask(__name__)
+app = Flask(__name__)  # Initialize Flask app
 
 
+# API start point for each market
 @app.route('/<tvsymbol>/start')
 def start_market(tvsymbol):
     try:
+        # Start market service for symboll
         result = tvapi.start_market_service(tvsymbol)
     except Exception as e:
+        # Return error message if market didnt start
         return jsonify({'error': str(e)}), 500
+    # Return success message if market started
     return jsonify({'response': result}), 200
 
 
+# API rsi point for each market
 @app.route('/<tvsymbol>/rsi')
 def get_rsi(tvsymbol):
     try:
+        # Get rsi value for symbol
         result = tvapi.get_rsi(tvsymbol)
     except InvalidSessionIdException as e:
+        # Return error message if session is timed out  - flags doesnt match session
         return jsonify({'error': str(e)}), 501
     except Exception as e:
+        # Return error message if rsi value not found (eg . market not started)
         return jsonify({'error': str(e)}), 500
+    # Return rsi json found
     return jsonify({'response': result}), 200
 
 
+# API stop point for each market
 @app.route('/<tvsymbol>/stop')
 def stop_market(tvsymbol):
     try:
+        # Stop market service for symbol
         result = tvapi.stop_market_service(tvsymbol)
     except InvalidSessionIdException as e:
+        # Return error message if session is timed out  - flags doesnt match session
         return jsonify({'error': str(e)}), 501
     except Exception as e:
+        # Return error message if rsi value not found (eg . market not started)
         return jsonify({'error': str(e)}), 500
+    # Return success message if market stopped
     return jsonify({'response': result}), 200
 
 
+# Run file
 if __name__ == '__main__':
-    tvapi = tv.API()
+    tvapi = tv.API()  # Initialize TradingView API
+    # Run Flask app, network accessable, port 5000, multi-threaded
     app.run(host="0.0.0.0", port=5000, threaded=True)
--- tradingview.py
+++ tradingview.py
@@ -1,15 +1,14 @@
-# Environment variables and datetime
-import re
+# Environment variables and datetime and regex
 import os
-import time
+import re
 from datetime import datetime
 # For web scraping
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import InvalidSessionIdException
 # For waiting for web elements to load instead of time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import InvalidSessionIdException
 # For MySQL database
 import json
 import pymysql
@@ -48,104 +47,147 @@ class API:
         self.browser_options = webdriver.ChromeOptions()
         self.browser_options.add_argument("--start-maximized")
 
+    # Start market service for symbol
     def start_market_service(self, __tvsymbol):
         try:
+            # Check if symbol is not supported, raise error
             if __tvsymbol not in self.tv_market_data:
                 raise Exception(
                     f"TradingView {__tvsymbol} is not supported. (start)")
+
+            # Check if flagged symbol is running and not timed out.
+            try:
+                # Check timezone button to see if session is timed out
+                self.tv_market_data[__tvsymbol]["instance"].find_element(
+                    By.XPATH, '//button[@data-name="time-zone-menu"]').text
+            # If session doesnt exsit, continue starting the service
+            except AttributeError as e:
+                pass
+            # If session is timed out, restart the flags, continue starting the service
+            except InvalidSessionIdException as e:
+                self.tv_market_data[__tvsymbol]["status"] = False
+                self.tv_market_data[__tvsymbol]["instance"] = None
+
+            # Check if flagged symbol is not running, start the service
             if not self.tv_market_data[__tvsymbol]["status"]:
+                # Change status to running
                 self.tv_market_data[__tvsymbol]["status"] = True
+                # Start selenium browser with needed options
                 self.tv_market_data[__tvsymbol]["instance"] = webdriver.Remote(
                     command_executor=self.selenium_url,
                     options=self.browser_options)
+                # Go to TradingView link for each symbol
                 self.tv_market_data[__tvsymbol]["instance"].get(
                     self.tv_market_data[__tvsymbol]["link"])
+                # Wait for Page to load
                 WebDriverWait(self.tv_market_data[__tvsymbol]["instance"], 20).until(
                     EC.presence_of_element_located((By.XPATH, '//div[@data-name="legend-source-item"]')))
-
                 self.tv_market_data[__tvsymbol]["instance"].implicitly_wait(1)
 
+                # Get session id for flagged symbol
                 temp_sessionid = str(
                     self.tv_market_data[__tvsymbol]["instance"].session_id)
-
+                # Return success message
                 return f"{__tvsymbol} service started. {temp_sessionid}"
+            # If flagged symbol is running, raise error
             else:
+                # Get session id for flagged symbol
                 temp_sessionid = str(
                     self.tv_market_data[__tvsymbol]["instance"].session_id)
                 raise Exception(
                     f"TradingView {__tvsymbol} service is already running. {temp_sessionid}")
+        # For redundant error handling, if anything happens, reset flags and raise error
         except InvalidSessionIdException as e:
             self.tv_market_data[__tvsymbol]["status"] = False
             self.tv_market_data[__tvsymbol]["instance"] = None
             raise InvalidSessionIdException(e)
 
+    # Get rsi value for symbol
     def get_rsi(self, __tvsymbol):
         try:
+            # Check if symbol is not supported, raise error
             if __tvsymbol not in self.tv_market_data:
                 raise Exception(
                     f"TradingView {__tvsymbol} is not supported. (get rsi)")
+
+            # Check if flagged symbol is running
             if self.tv_market_data[__tvsymbol]["status"]:
+                # Get raw string value of time, rsi, price from TradingView
                 self.raw_time = self.tv_market_data[__tvsymbol]["instance"].find_element(
                     By.XPATH, '//button[@data-name="time-zone-menu"]').text.split("\n")[-1]
-
                 self.raw_rsi_value = self.tv_market_data[__tvsymbol]["instance"].find_element(
                     By.XPATH, '//div[@data-name="legend-source-item"]').text.split("\n")[-1]
-
                 self.raw_price_price = self.tv_market_data[__tvsymbol]["instance"].find_element(
                     By.XPATH, '//div[@data-name="legend-series-item"]').text.split("\n")[-1]
 
+                # Format and filter raw time string into datetime
                 self.time_step1 = self.raw_time.split(" ")[0]
                 self.time_step2 = datetime.strptime(
                     self.time_step1, "%H:%M:%S").time()
                 self.datetime_value = str(datetime.combine(
                     datetime.now().date(), self.time_step2))
 
+                # Format and filter raw rsi string into float
                 self.rsi_value = float(self.raw_rsi_value)
 
-                self.price_step1 = [float(value)
-                                    for value in re.findall('\d+\.\d+', self.raw_price_price)][0:4]
-
+                # Format and filter raw price string into dict using regex
+                self.price_step1 = [
+                    float(value) for value in re.findall(
+                        '\d+\.\d+', self.raw_price_price)][0:4]
                 self.price_values = {
                     "open": self.price_step1[0],
                     "high": self.price_step1[1],
                     "low": self.price_step1[2],
-                    "close": self.price_step1[3]
-                }
+                    "close": self.price_step1[3]}
 
-                time.sleep(1)
+                # Get session id for flagged symbol
                 temp_sessionid = str(
                     self.tv_market_data[__tvsymbol]["instance"].session_id)
-                self.final_data = {
+
+                # Return values
+                return {
                     "rsi": self.rsi_value,
                     "datetime": self.datetime_value,
                     "tv_symbol": __tvsymbol,
                     "price": self.price_values,
                     "sessionid": temp_sessionid
                 }
-
-                return self.final_data
+            # If flagged symbol is not running, raise error
             else:
                 raise Exception(
                     f"TradingView {__tvsymbol} service is not running, can not get rsi.")
+        # If session is timed out, reset flags, raise error
         except InvalidSessionIdException as e:
             self.tv_market_data[__tvsymbol]["status"] = False
             self.tv_market_data[__tvsymbol]["instance"] = None
             raise InvalidSessionIdException(e)
 
+    # Stop market service for symbol
     def stop_market_service(self, __tvsymbol):
         try:
+            # Check if symbol is not supported, raise error
             if __tvsymbol not in self.tv_market_data:
                 raise Exception(
                     f"TradingView {__tvsymbol} is not supported. (stop)")
+
+            # Check if flagged symbol is running
             if self.tv_market_data[__tvsymbol]["status"]:
+                # Get session id for flagged symbol (before quitting)
                 temp_sessionid = str(
                     self.tv_market_data[__tvsymbol]["instance"].session_id)
+
+                # Quit selenium and reset flags
                 self.tv_market_data[__tvsymbol]["instance"].quit()
                 self.tv_market_data[__tvsymbol]["status"] = False
+                self.tv_market_data[__tvsymbol]["instance"] = None
+
+                # Return success message
                 return f"{__tvsymbol} service stopped. {temp_sessionid}"
+            # If flagged symbol is not running, raise error
             else:
                 raise Exception(
                     f"TradingView {__tvsymbol} service is not running.")
+        # If session is timed out, reset flags, raise error
         except InvalidSessionIdException as e:
             self.tv_market_data[__tvsymbol]["status"] = False
             self.tv_market_data[__tvsymbol]["instance"] = None
